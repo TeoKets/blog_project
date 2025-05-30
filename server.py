6dbc019e0f6b6e76b76e9bcb312d4b1b7de2ccfb
Theofilos Ketsetzidis <ketsetzidis909@gmail.com>
2025-05-30 06:35:05 +0300

    skills: flask,jinja,git,github,API

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
+from email.feedparser import headerRE
+from operator import itemgetter
+
 import requests
 from pprint import pprint
-from flask import Flask,render_template,url_for,redirect
+from flask import Flask,render_template,url_for,redirect,request
 
 
 respond=requests.get("https://api.npoint.io/c8f2cc570fd551ae00f1")
@@ -20,9 +23,19 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
+
+#i dont get the info yet
+@app.route("/contact",methods=['GET','POST'])
 def contact():
-    return render_template("contact.html")
+    if request.method=="GET":
+        return render_template("contact.html")
+    else:
+
+        data = request.form
+
+        return render_template("contact.html",nav="success")
+
+
 
 
 
